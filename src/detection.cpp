5443aea0e99ae9d53f8a8f19d730f2985e84bb03
Michael Strobel <michael@kryops.de>
2019-05-01 07:51:42 +0200

    Try FromJust() instead of ToChecked() nan API to restore support for older Node versions

--- src/detection.cpp
+++ src/detection.cpp
@@ -119,8 +119,8 @@ void Find(const Nan::FunctionCallbackInfo<v8::Value>& args) {
 
 	if (args.Length() == 3) {
 		if (args[0]->IsNumber() && args[1]->IsNumber()) {
-			vid = (int) Nan::To<int>(args[0]).ToChecked();
-			pid = (int) Nan::To<int>(args[1]).ToChecked();
+			vid = (int) Nan::To<int>(args[0]).FromJust();
+			pid = (int) Nan::To<int>(args[1]).FromJust();
 		}
 
 		// callback
@@ -133,7 +133,7 @@ void Find(const Nan::FunctionCallbackInfo<v8::Value>& args) {
 
 	if (args.Length() == 2) {
 		if (args[0]->IsNumber()) {
-			vid = (int) Nan::To<int>(args[0]).ToChecked();
+			vid = (int) Nan::To<int>(args[0]).FromJust();
 		}
 
 		// callback
